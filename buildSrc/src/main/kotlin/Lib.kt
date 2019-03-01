638f583c33103ff364d1fbccad7d2c3b779efd51
Björn Petersen <pheasn@gmail.com>
2019-03-01 10:02:47 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,13 +1,11 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val KOTLIN_LOGGING = "1.6.24"
+    const val KOTLIN_LOGGING = "1.6.25"
 
-    const val MUSICBOT = "0.18.0"
+    const val MUSICBOT = "0.18.1"
 
     const val SPOTIFY = "2.1.2"
     const val OAUTHCLIENT = "1.28.0"
 
     const val JUNIT = "5.4.0"
-    const val MOCK_K = "1.9.1"
-    const val ASSERT_J = "3.11.1"
 }
