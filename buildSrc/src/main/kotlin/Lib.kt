5f5f9ecb9a157ab62ba108f73e0f77750fcb2b24
Björn Petersen <pheasn@gmail.com>
2019-05-05 17:31:53 +0200

    Update Ktor and Kotlin versions

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -4,7 +4,7 @@ object Lib {
     const val MUSICBOT = "0.20.0"
 
     const val SPOTIFY = "2.1.2"
-    const val KTOR = "1.1.3"
+    const val KTOR = "1.1.4"
 
     const val SLF4J = "1.8.0-beta4"
     const val JUNIT = "5.4.2"
